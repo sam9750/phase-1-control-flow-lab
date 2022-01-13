9b6172eb4a24d832336bcdcc5f6eea2c0ea00778
sam9750 <sam.wadhwa95@gmail.com>
2022-01-13 00:01:42 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) 
+  return "This one is on me!";
+  else if (feet < 2500 && 2000) 
+  return "I will gladly take your thirty bucks.";
+  else if (feet > 2500) return "No can do.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city) {
+  // const city = 'NYC' 
+  const approval = (city === 'NYC') ? "Ok, sounds good.": "No go.";
+  return(approval);
+  
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  
 }
+ 
+
+
+
+function switchOnCharmFromTip(amountTipped) {
+
+  let reaction;
+
+ switch(amountTipped) {
+
+    case 'generous':
+        reaction = "Thank you so much.";
+        break;
+    case 'not as generous': 
+        reaction = "Thank you."
+        break;
+    default:
+        return "Bye."
+  
+}
+return(reaction);
+ }
